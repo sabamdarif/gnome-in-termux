29c40b38d6ad1fef346d03b6a5fb8ddf1a357ced
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2024-03-19 19:04:40 +0530

    add termux x11 setup
    add option to setup termux x11 for gui , fix some issues , fix chromium  install issue in ubuntu

--- setup.sh
+++ setup.sh
@@ -15,33 +15,37 @@ printf "\033[33m┃┃╭━┫╭╮┫╭╮┃╰╯┃┃━┫╱┃┃┃�
 printf "\033[33m┃╰┻━┃┃┃┃╰╯┃┃┃┃┃━┫╭┫┣┫┃┃┃╱╱┃┃┃┃━┫┃┃┃┃┃╰╯┣╋╋╮\033[0m\n"
 printf "\033[33m╰━━━┻╯╰┻━━┻┻┻┻━━╯╰━━┻╯╰╯╱╱╰╯╰━━┻╯╰┻┻┻━━┻╯╰╯\033[0m\n"
 printf "\033[32m code by @sabamdrif \033[0m\n"
-printf "\033[32m subscribe my YouTube Channel Hello Android \033[0m\n"                                                     
+printf "\033[32m subscribe my YouTube Channel Hello Android \033[0m\n"  
+echo                                                   
 }
 
 questions() {
 	banner
 	echo 
-	echo "${G} Select Distro "${W}
+	echo "${R} [${W}-${R}]${G} Select Distro "${W}
 	echo
 	echo "${Y}1. Debian (Default)"${W}
 	echo
 	echo "${Y}2. Ubuntu"${W}
 	echo
-    echo "${Y}2. Kali"${W}
+    echo "${Y}3. Kali"${W}
 	echo
 	read -p "${Y}select a distro: "${W} answer_distro
 	echo
+    banner
+    read -p "${R} [${W}-${R}]${Y}Do you want to setup termux-x11(y/n) "${W} tx11_answer
 }
 
 basic_task() {
     banner
-    echo "${G} Updating Termux... "${W}
+    echo "${R} [${W}-${R}]${G} Updating Termux... "${W}
+    echo
     pkg update -y
     clear
-    echo "${G} Setting Up Storage... "${W}
+    echo "${R} [${W}-${R}]${G} Setting Up Storage... "${W}
     termux-setup-Storage
     clear
-    echo "${G} Installling Required Packages... "${W}
+    echo "${R} [${W}-${R}]${G} Installling Required Packages... "${W}
      packs=(wget proot proot-distro pulseaudio)
     for pack in "${packs[@]}"; do
         type -p "$pack" &>/dev/null || {
@@ -58,7 +62,7 @@ install_distro() {
     elif [[ ${answer_distro} == "2" ]]; then
         proot-distro install ubuntu
     elif [[ ${answer_distro} == "3" ]]; then
-    echo "${G} Using Modded-Kali Script To Install Kali In Termux..."${W}
+    echo "${R} [${W}-${R}]${G} Using Modded-Kali Script To Install Kali In Termux..."${W}
     sleep 0.5
     wget -O kaliinstaller.sh https://raw.githubusercontent.com/sabamdarif/modded-kali/main/setup.sh ; bash kaliinstaller.sh 
     rm kaliinstaller.sh
@@ -72,7 +76,7 @@ setup_installer() {
     distro_path="/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs"
     echo "${G} Setup Installer... "${W}
     cd ~
-    wget -O gnome-installer.sh https://raw.githubusercontent.com/sabamdarif/gnome-in-termux/main/install-gnome-desktop
+   wget -O gnome-installer.sh https://raw.githubusercontent.com/sabamdarif/gnome-in-termux/main/install-gnome-desktop
     if [[ ${answer_distro} == "1" ]]; then
         mv gnome-installer.sh $distro_path/debian/root
         proot-distro login debian -- /bin/sh -c 'bash gnome-installer.sh'
@@ -88,7 +92,16 @@ setup_installer() {
     fi
 }
 
+setup_tx11() {
+    if [ "$tx11_answer" == "y" ]; then
+    pkg install x11-repo
+    pkg install termux-x11-nightly
+    sed -i 's/tx11_setup_answer/y/g' /data/data/com.termux/files/home/gnome-installer.sh
+    fi
+}
+
 questions
 basic_task
 install_distro
+setup_tx11
 setup_installer
